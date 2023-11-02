a9024745fb67b102d017ae92c483d0b29a0b29ed
tim1508 <149669572+tim1508@users.noreply.github.com>
2023-11-02 13:38:27 +0100

    s

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.title('FCS Group Project of Group 6.2 :rocket')
+st.title('FCS Group Project of Group 6.2 :rocket:')
 st.markdown("Welcome to our web application that is still to be built")
