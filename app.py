4ba3d3a15618051fb523e99d564cce621fa9aedc
tibauld <150721809+tibauld@users.noreply.github.com>
2023-11-30 16:41:03 +0100

    Update Tibauld

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ import streamlit as st
 import pandas as pd
 import sqlite3
 import re
+from PIL import image
+
+# Implementing global Variables for CAPTCHA
+length_captcha = 4
+width = 200
+height = 150
 
 # Create a SQLite database connection
 conn = sqlite3.connect('hsg_reporting.db')
@@ -21,6 +27,48 @@ c.execute('''
 ''')
 conn.commit()
 
+# Beginn des hinzugefügten CAPTCHA-Codes
+# ----------------------------------------------------------------------------------
+# Hier definieren Sie die captcha_control Funktion
+def captcha_control():
+    # control if the captcha is correct
+    if 'controllo' not in st.session_state or st.session_state['controllo'] == False:
+        st.title("Captcha Control on Streamlit🤗")
+        
+        # define the session state for control if the captcha is correct
+        st.session_state['controllo'] = False
+        col1, col2 = st.columns(2)
+        
+        # define the session state for the captcha text because it doesn't change during refreshes 
+        if 'Captcha' not in st.session_state:
+            st.session_state['Captcha'] = ''.join(random.choices(string.ascii_uppercase + string.digits, k=length_captcha))
+        print("the captcha is: ", st.session_state['Captcha'])
+        
+        # setup the captcha widget
+        image = ImageCaptcha(width=width, height=height)
+        data = image.generate(st.session_state['Captcha'])
+        col1.image(data)
+        captcha_text = col2.text_input('Enter captcha text here')
+        
+        # verify captcha
+        if col2.button("Verify"):
+            if st.session_state['Captcha'].lower() == captcha_text.lower().strip():
+                # Captcha is correct, set the flag to True
+                st.session_state['controllo'] = True
+                st.experimental_rerun() 
+            else:
+                # Captcha is incorrect, show an error
+                st.error("🚨 The captcha code is incorrect, please try again")
+                # Reset the captcha to force the user to try again
+                del st.session_state['Captcha']
+                del st.session_state['controllo']
+                st.experimental_rerun()
+    else:
+        # If the captcha is already solved, do nothing
+        pass
+# Ende des hinzugefügten CAPTCHA-Codes
+# ----------------------------------------------------------------------------------
+
 def submission_form():
     st.header("HSG Reporting Tool - Submission Form")
 
@@ -30,16 +78,20 @@ def submission_form():
 
     # Check wheter the specified email address is a real hsg mail address
     def is_valid_email(hsg_email):
-        if hsg_email:
-            hsg_email_pattern = r'^[\w.]+@unisg\.ch$'
-            match = re.match(hsg_email_pattern, hsg_email)
-            return bool(match)
-        else:
-            return True
+        hsg_email_pattern = r'^[\w.]+@(student\.)?unisg\.ch$'
+        match = re.match(hsg_email_pattern, hsg_email)
+        return bool(match)
 
     # Returning an error when the mail address is invalid
     if not is_valid_email(hsg_email):
-        st.error("Invalid mail address. Please check that you have entered your HSG mail address correctly.")
+        st.error("Invalid mail address. Please check that you have entered your hsg mail address correctly.")
+    
+    # File uploader for photos
+    uploaded_file = st.file_uploader("Upload a photo", type=["jpg", "jpeg", "png"])
+
+    # Display the uploaded foalder
+    if uploaded_file is not None:
+        st.image(uploaded_file, caption="Uploaded Photo", use_column_with=True)
 
     # Room number input
     room_number = st.text_input("Room Number:")
@@ -51,6 +103,7 @@ def submission_form():
             width="100%" height="420" frameborder="0" marginheight="0" marginwidth="0"
             scrolling="no"></iframe>
     """, unsafe_allow_html=True)
+
     
     # Issue Type checkboxes
     st.subheader("Issue Type:")
@@ -63,26 +116,33 @@ def submission_form():
     
     # Wenn der Benutzer auf "Submit" klickt
     if st.button("Submit"):
-        selected_issue_types = []
-        if it_problem:
-            selected_issue_types.append("IT Problem")
-        if missing_material:
+        # CAPTCHA-Validierung
+        if 'controllo' not in st.session_state or st.session_state['controllo'] == False:
+            captcha_control()
+            st.stop()  # Stoppt die Ausführung des weiteren Codes, bis das CAPTCHA gelöst ist
+    
+    # Wenn das CAPTCHA gelöst ist, verarbeiten Sie die eingegebenen Daten
+        else:
+            selected_issue_types = []
+            if it_problem:
+                selected_issue_types.append("IT Problem")
+            if missing_material:
                 selected_issue_types.append("Missing Material")
-        if non_functioning_facilities:
+            if non_functioning_facilities:
                 selected_issue_types.append("Non-functioning Facilities")
-        issue_types = ', '.join(selected_issue_types)
+            issue_types = ', '.join(selected_issue_types)
         
         # Daten in die Datenbank einfügen
-        c.execute('''
+            c.execute('''
                 INSERT INTO submissions (name, hsg_email, issue_type, room_number, importance)
                 VALUES (?, ?, ?, ?, ?)
             ''', (name, hsg_email, issue_types, room_number, importance))
-        conn.commit()
-        st.success("Submission Successful!")
+            conn.commit()
+            st.success("Submission Successful!")
 
         if not (name and hsg_email and room_number and importance and (it_problem or missing_material or non_functioning_facilities)):
             st.error("Please fill out all fields before submitting.")
-            if not is_valid_email(hsg_email):
+            if not is_valid_hsg_email(hsg_email):
                 st.error("Invalid mail address. Please enter your HSG-address.")
 
             else:
@@ -143,3 +203,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+ 
